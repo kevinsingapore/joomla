cc7dc0dddcbf0a98e8e20f6549c6cad6d2d21656
fuqiang <948876386@qq.com>
2019-09-02 15:52:36 +0800

    detect installation file

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # joomla
 version:3.9.11
+# 创建数据目录
+mkdir -p /home/joomla/var/www/html
+chmod -R 777 /home/joomla/var/www/html
+# 创建数据库
+创建数据库文档地址https://www.xiaodianer.net/index.php/docker/2-joomla-mysql
+# 运行安装shell脚本
+bash install-joomla.sh 
+# 查看docker实例是否正常运行
+docker ps
--- install-joomla.sh
+++ install-joomla.sh
@@ -5,8 +5,8 @@ docker run -itd \
            -p 8080:80 \
            --restart always \
            -v /home/joomla/var/www/html:/var/www/html \
-           -e JOOMLA_DB_HOST=172.31.219.12:3306 \
-           -e JOOMLA_DB_NAME=joomladb \
-           -e JOOMLA_DB_USER=joomla \
-           -e JOOMLA_DB_PASSWORD=****333 \
+           -e JOOMLA_DB_HOST=<ip>:3306 \
+           -e JOOMLA_DB_NAME=<database_name> \
+           -e JOOMLA_DB_USER=<database_username> \
+           -e JOOMLA_DB_PASSWORD=<database_password> \
            docker.io/joomla:3.9.11
